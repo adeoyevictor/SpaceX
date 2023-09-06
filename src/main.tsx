7244642f6f12d08185beb270fd23f8212073766e
Adeoye Victor <79271707+adeoyevictor@users.noreply.github.com>
2023-09-06 17:28:03 +0100

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -5,7 +5,6 @@ import { BrowserRouter as Router } from 'react-router-dom';
 import { UserContextProvider } from './context/UserContext.tsx';
 import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
 
-// Create an Apollo Client instance and configure it with your API endpoint
 const client = new ApolloClient({
   uri: 'https://spacex-production.up.railway.app/',
   cache: new InMemoryCache(),
